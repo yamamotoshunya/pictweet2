4e97d850e27ddbdbaff0137278a801745f0822bc
EC2 Default User <ec2-user@ip-172-31-35-119.ap-northeast-1.compute.internal>
2021-01-10 06:35:37 +0000

    save

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,55 @@
+class TweetsController < ApplicationController
+  
+   before_action :move_to_index, except: [:index, :show]
+
+  
+  def index
+     @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+  end
+  
+  def new
+    
+  end
+  
+  def create
+   Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
+  end
+  
+  def destroy
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.destroy
+    end
+  end
+  
+   def show
+    @tweet = Tweet.find(params[:id])
+    @comments = @tweet.comments.includes(:user)
+   end
+  
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  
+  def update
+    tweet = Tweet.find(params[:id])
+   if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+  end
+  end
+  
+  private
+  def tweet_params
+    params.permit(:image, :text)
+  end
+  
+  def move_to_index
+    unless user_signed_in?
+      puts "ログインしてください"
+      redirect_to   action:  :index
+    end
+    
+  end
+  
+end
